8ac90581c17f900ef5fb13846859aeaac19e3621
Dawid Minorczyk <minorcz2@illinois.edu>
2017-07-22 18:09:31 -0700

    Have a functioning WikiPedia extraction script. Bit too much code and still not fully functional. Good enough to do the job.

--- Scraper/scrape_wiki.py
+++ Scraper/scrape_wiki.py
@@ -1,17 +1,3 @@
-
-# Common library imports
-import re
-import os
-import sys
-
-# Command line parse imports
-import argparse
-import scrape_wiki_docs as docs
-
-# XML and HTML parsing imports
-import requests
-from bs4 import BeautifulSoup
-
 # Data manipulation import
 import numpy as np
 import pandas as pd
@@ -19,33 +5,24 @@ import pandas as pd
 # Custom parsing import
 from wikitable import WikiTable
 
-def create_parser():
-    '''
-    '''
-    parser = argparse.ArgumentParser(description = docs.help_text)
-    parser.add_argument('-u', '--url', type = str, help = docs.help_url_text)
-    parser.add_argument('-t', '--table', type = int, help = docs.help_tag_text)
+def main():
 
-    return parser
+    URL = 'https://en.wikipedia.org/wiki/List_of_stations_owned_or_operated_by_Sinclair_Broadcast_Group'
 
-
-def main(argv):
-    '''
-    This function is automatically run if this script is called from the command line. It does the 
-    following:
-
-        1. Parses command line inputs for URLs and table tag
-        2. Connects to the given URL and extracts the table
-        3. Saves the table into csv format
-    '''
-    # Parse input arguments
-    parser = create_parser()
-    inputs = parser.parse_args(argv)
-
-    sinclair_table = WikiTable(url = inputs.url, tab_num = inputs.table)
-    table_df = sinclair_table.pandas_from_url(col_th = True)
+    stations_table = WikiTable(
+        tab_num = 0, 
+        regex_ex = ['Transmitter.*'], 
+        regex_pos = [(0, 1)]
+    )
+    sinclair_table = WikiTable(
+        url = URL, 
+        tab_num = 1,
+        col_ref = [1],
+        col_th = True,
+        on_link = stations_table
+    )
+    table_df = sinclair_table.pandas_from_url()
     table_df.to_csv('sinclair_stations.csv', index = False)
 
-
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
